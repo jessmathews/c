d862353b8c76d78a47ef1eb41724a53c4f57d7e0
Jess <81862618+jessmathews@users.noreply.github.com>
2023-07-03 20:06:33 +0530

    Create dec2bin.c

---
+++ dec2bin.c
@@ -0,0 +1,21 @@
+#include <stdio.h>    
+#include <stdlib.h>  
+
+
+int main(){  
+int a[8],n,i;    
+system ("cls");  
+printf("Enter a decimal:");    
+scanf("%d",&n);    
+for(i=0;n>0;i++)    
+{    
+a[i]=n%2;    
+n=n/2;    
+}    
+printf("Binary of %d is:",n);    
+for(i=i-1;i>=0;i--)    
+{    
+printf("%d",a[i]);    
+}    
+return 0;  
+}  
